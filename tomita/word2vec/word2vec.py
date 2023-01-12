913f50ab9e4ab300edf4683e0e560844d3d0160f
Kovalev Ivan <vania8022@gmail.com>
2023-01-12 18:32:15 +0300

    added tomita parser and MarkedNews colection to database. Start work with w2v

---
+++ tomita/word2vec/word2vec.py
@@ -0,0 +1,89 @@
+# -*- encoding: utf-8 -*-
+import os
+import sys
+import re
+from pymongo import MongoClient
+import string
+
+
+from pyspark.ml.feature import Word2VecModel
+from pyspark.sql import SparkSession
+
+from pyspark.ml.feature import Tokenizer
+from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.feature import CountVectorizer
+from pyspark.ml.feature import IDF
+from pyspark.ml.feature import Word2Vec
+
+
+spark = SparkSession\
+        .builder \
+        .appName("Word2VecApplication") \
+        .getOrCreate()
+
+
+if not os.path.exists('/word2vec_model'):
+    print("START")
+
+    if not os.path.exists('/news/0.txt'):
+
+        # start mongodb
+        client = MongoClient(host='localhost',
+                             port=27017,
+                             username='root',
+                             password='root123')
+
+        # get news from database
+        db = client['parser']
+        collection = db.News
+        raw_news = collection.find()
+
+        i = 0
+        for news in raw_news:
+            f = open(f'./news/{i}.txt', 'w', encoding='utf-8')
+            text = news['title'] + '. ' + news['body']
+            f.write(text)
+            print(i)
+            i += 1
+            f.close()
+
+
+    # Построчная загрузка файла в RDD
+    input_file = spark.sparkContext.wholeTextFiles('/word2vec/news/*.txt')
+    prepared = input_file.map(lambda x: (x[0], x[1].translate(str.maketrans('', '', string.punctuation))))
+    prepared_df = prepared.toDF().selectExpr('_1 as path', '_2 as text')
+
+    # Разбить на токены
+    tokenizer = Tokenizer(inputCol='text', outputCol='words')
+    words = tokenizer.transform(prepared_df)
+
+    # Удалить стоп-слова
+    stop_words = StopWordsRemover.loadDefaultStopWords('russian')
+    remover = StopWordsRemover(inputCol='words', outputCol='filtered', stopWords=stop_words)
+    filtered = remover.transform(words)
+
+    word2Vec = Word2Vec(inputCol='filtered', outputCol='result')
+    model = word2Vec.fit(filtered)
+    w2v_df = model.transform(filtered)
+    w2v_df.show()
+    model.save("/word2vec/word2vec_model")
+
+    #spark.stop()
+
+
+model = Word2VecModel.load('/word2vec/word2vec_model')
+
+
+while True:
+    try:
+        entry_word = input("Введите слово для поиска синонимов:")
+        if entry_word == "-x":
+            break
+        entry_word = entry_word.replace(' ', '')
+        entry_word = entry_word.lower()
+        model.findSynonyms(entry_word, 30).show()
+    except Exception as ex:
+        print("Данного слова нет в словаре!")
+        print(ex)
+
+spark.stop()
